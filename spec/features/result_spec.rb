afec8e86bc82236ff3ddb9bd9f0514611f41c9a0
naridas <gregsparta@gmail.com>
2016-05-03 11:08:35 +0100

    simple fixes

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -7,29 +7,25 @@ feature 'Results of RPS' do
   end
 
   scenario 'see what Computer selected' do
-    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
-    sign_in_and_play
+    computer_choice
     click_button 'Rock'
     expect(page).to have_content 'Computer chose Scissors'
   end
 
   scenario 'win' do
-    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
-    sign_in_and_play
+    computer_choice
     click_button 'Rock'
     expect(page).to have_content 'You win!'
   end
 
   scenario 'lose' do
-    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
-    sign_in_and_play
+    computer_choice
     click_button 'Paper'
     expect(page).to have_content 'You lose!'
   end
 
-  scenario 'lose' do
-    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
-    sign_in_and_play
+  scenario 'draw' do
+    computer_choice
     click_button 'Scissors'
     expect(page).to have_content 'You draw!'
   end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -3,3 +3,7 @@ def sign_in_and_play
   fill_in "player_name", :with => "Grig"
   click_button "Submit"
 end
+def computer_choice
+  allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+  sign_in_and_play
+end
